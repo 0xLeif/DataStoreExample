5d8a3d6cf9ed4b45becf54a240eb9f3f04c24b3d
Leif <zmeriksen@icloud.com>
2023-07-22 14:56:54 -0600

    Initial Commit

---
+++ DataStoreExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DataStoreExample
+//
+//  Created by Leif on 7/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataStoreExample/DataStoreExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStoreExampleApp.swift
+//  DataStoreExample
+//
+//  Created by Leif on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DataStoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
